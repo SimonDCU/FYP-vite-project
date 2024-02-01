2ccc26e85450920a6144343ee6d0e5435180d102
SimonDCU <simon.kelly292@mail.dcu.ie>
2024-02-01 11:37:20 +0000

    reverting change in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   server: { https: true },
-  base: '/FYP-vite-project/dist',
+  base: '/FYP-vite-project/',
   plugins: [ mkcert() ],
 })
