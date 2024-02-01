cf635059115d8e9a3802fed010d84f152ff243e2
SimonDCU <simon.kelly292@mail.dcu.ie>
2024-02-01 11:37:58 +0000

    testing change in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   server: { https: true },
-  base: '/FYP-vite-project/',
+  base: '/FYP-vite-project/dist',
   plugins: [ mkcert() ],
 })
