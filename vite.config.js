0582fbeb6a5000bc63297823e2ba5c97f7e65f46
SimonDCU <simon.kelly292@mail.dcu.ie>
2024-02-01 10:12:41 +0000

    Enabled vite-build ability to incorporate .stl files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import mkcert from 'vite-plugin-mkcert'
 export default defineConfig({
   server: { https: true },
   base: '/FYP-vite-project/',
-  plugins: [ mkcert() ]
+  plugins: [ mkcert() ],
+  assetsInclude: ['**/*.stl']
 })
